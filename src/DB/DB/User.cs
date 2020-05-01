89dd4864d381bae788e6a14fb0e2876acf6c894c
matt nolan <matthewnolan.nolan@gmail.com>
2020-05-01 15:40:27 +0100

    src update

---
+++ src/DB/DB/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DB
+{
+    public class User
+    {
+        public String UserName { get; set; }
+
+        public String PassWord { get; set; }
+    }
+}
